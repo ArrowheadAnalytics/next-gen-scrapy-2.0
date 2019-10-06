bbe8d906931d34e99e355639ea6f1a386006e663
903124 <szeyuihk25@gmail.com>
2019-10-06 21:30:25 +0800

    add argparse

--- test.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='Process some integers.')
-
-parser.add_argument('-s', '--season', nargs='+', type=str,dest='season',default=['2019'], help='input season type')
-
-args = parser.parse_args()
-
-print(args.season)
